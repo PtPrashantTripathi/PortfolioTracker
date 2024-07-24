8dbc39f8a24ac8a9130728b81e2f8a018009d744
Prashant Tripathi <Prashant.Tripathi@unilever.com>
2024-07-24 15:37:53 +0530

    updated

--- NOTEBOOKS/common_utilities.py
+++ NOTEBOOKS/common_utilities.py
@@ -1,6 +1,9 @@
+import os
 import re
 import time
+import pathlib
 import requests
+from enum import Enum
 from datetime import datetime
 
 
@@ -56,3 +59,137 @@ def get_stock_price_data(name, from_date, to_date):
     else:
         # Raise an exception if the request failed
         response.raise_for_status()
+
+
+class Portfolio:
+    def __init__(self):
+        self.stocks = dict()
+
+    def trade(self, record: dict = None):
+        stock_name = str(record.get("stock_name"))
+
+        if stock_name not in self.stocks:
+            self.stocks[stock_name] = Stock(stock_name)
+
+        record.update(
+            self.stocks[stock_name].trade(
+                side=str(record.get("side")).upper(),
+                amount=float(record.get("amount")),
+                quantity=int(record.get("quantity")),
+                price=float(record.get("price")),
+            )
+        )
+        return record
+
+
+class Stock:
+    def __init__(self, stock_name):
+        self.stock_name = stock_name
+        self.holding_price = 0
+        self.holding_quantity = 0
+        self.holding_amount = 0
+
+    def trade(
+        self,
+        side: str,
+        amount: float,
+        quantity: int,
+        price: float,
+    ):
+        buy_price = 0
+        buy_quantity = 0
+        buy_amount = 0
+        sell_price = 0
+        sell_quantity = 0
+        sell_amount = 0
+
+        if side == "BUY":
+            buy_price = price
+            buy_quantity = quantity
+            buy_amount = amount
+            self.holding_quantity += buy_quantity
+            self.holding_amount += buy_amount
+        elif side == "SELL":
+            sell_price = price
+            sell_quantity = quantity
+            sell_amount = amount
+            buy_price = self.holding_price
+            buy_amount = quantity * buy_price
+            self.holding_quantity -= sell_quantity
+            self.holding_amount -= buy_amount
+        else:
+            raise Exception(f"{side} was never excepected")
+
+        # Update the holding price only if there is remaining quantity
+        if self.holding_quantity != 0:
+            self.holding_price = self.holding_amount / self.holding_quantity
+
+        return {
+            "buy_price": buy_price,
+            "buy_quantity": buy_quantity,
+            "buy_amount": buy_amount,
+            "sell_price": sell_price,
+            "sell_quantity": sell_quantity,
+            "sell_amount": sell_amount,
+            "holding_price": self.holding_price,
+            "holding_quantity": self.holding_quantity,
+            "holding_amount": self.holding_quantity,
+        }
+
+
+# Current Working Dirctory Path
+cwd = pathlib.Path(os.getcwd())
+if cwd.name != "Upstox":
+    cwd = cwd.parent
+
+
+def global_path(source_path):
+    """
+    funcation to generate file path
+    """
+    data_path = cwd.joinpath("DATA").joinpath(source_path).resolve()
+    data_path.parent.mkdir(parents=True, exist_ok=True)
+    return data_path
+
+
+class GlobalPath(Enum):
+    """
+    Global Paths ENUM
+    """
+
+    # Current Working Dirctory Path
+    CWD = cwd
+
+    # TradeHistory
+    TRADEHISTORY_BRONZE_LAYER_PATH = global_path("BRONZE/TradeHistory")
+    TRADEHISTORY_SILVER_LAYER_PATH = global_path("SILVER/TradeHistory")
+    TRADEHISTORY_GOLD_LAYER_PATH = global_path("GOLD/TradeHistory")
+    TRADEHISTORY_SILVER_FILE_PATH = global_path(
+        "SILVER/TradeHistory/TradeHistory_data.csv"
+    )
+    TRADEHISTORY_GOLD_FILE_PATH = global_path("GOLD/TradeHistory/TradeHistory_data.csv")
+
+    # BillSummary
+    BILLSUMMARY_BRONZE_LAYER_PATH = global_path("BRONZE/BillSummary")
+    BILLSUMMARY_SILVER_LAYER_PATH = global_path("SILVER/BillSummary")
+    BILLSUMMARY_SILVER_FILE_PATH = global_path(
+        "SILVER/BillSummary/BillSummary_data.csv"
+    )
+
+    # StockPrice
+    STOCKPRICE_BRONZE_LAYER_PATH = global_path("BRONZE/StockPrice")
+    STOCKPRICE_SILVER_LAYER_PATH = global_path("SILVER/StockPrice")
+    STOCKPRICE_SILVER_FILE_PATH = global_path("SILVER/StockPrice/StockPrice_data.csv")
+
+    # Symbol
+    SYMBOL_BRONZE_LAYER_PATH = global_path("BRONZE/Symbol")
+    SYMBOL_SILVER_LAYER_PATH = global_path("SILVER/Symbol")
+    SYMBOL_SILVER_FILE_PATH = global_path("SILVER/Symbol/Symbol_data.csv")
+
+    # ProfitLoss
+    PROFITLOSS_GOLD_LAYER_PATH = global_path("GOLD/ProfitLoss")
+    PROFITLOSS_GOLD_FILE_PATH = global_path("GOLD/ProfitLoss/ProfitLoss_data.csv")
+
+    # Holdings
+    HOLDINGS_GOLD_LAYER_PATH = global_path("GOLD/Holdings")
+    HOLDINGS_GOLD_FILE_PATH = global_path("GOLD/Holdings/Holdings_data.csv")
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 **Description:**
 
-I've just completed an exciting ETL journey, leveraging the power of Medallion architecture! 🚀 This Python notebook seamlessly extracts, transforms, and loads data from my Upstox trading account into a multi-tiered storage system: Bronze, Silver, and Gold layers. It's a powerful way to enhance data management, analytics, and decision-making in my trading activities.
+I've just completed an exciting ETL journey, leveraging the power of Medallion architecture! 🚀 This Python notebook seamlessly extracts, transforms, and loads data from my Upstox trading account into a multi-tiered storage system: BRONZE, SILVER, and GOLD layers. It's a powerful way to enhance data management, analytics, and decision-making in my trading activities.
 
 **Key Highlights:**
 
@@ -12,9 +12,9 @@ I've just completed an exciting ETL journey, leveraging the power of Medallion a
 
 🛠️ **ETL Workflow**: Perform data extraction, apply transformations, and load the processed data into multiple layers to facilitate various analytics and reporting needs.
 
-📈 **Bronze, Silver, Gold Layers**: Structured storage tiers for raw data (Bronze), cleaned and transformed data (Silver), and curated data ready for analysis (Gold).
+📈 **BRONZE, SILVER, GOLD Layers**: Structured storage tiers for raw data (BRONZE), cleaned and transformed data (SILVER), and curated data ready for analysis (GOLD).
 
-📊 **Presentation Layer**: Another companion notebook creates interactive visualizations and insights from the Gold layer data, making it easier to understand my trading performance and make informed decisions.
+📊 **Presentation Layer**: Another companion notebook creates interactive visualizations and insights from the GOLD layer data, making it easier to understand my trading performance and make informed decisions.
 
 📓 **Educational Tool**: A valuable resource for traders looking to optimize their data analysis workflow.
 
