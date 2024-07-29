4c4c39746a48c6f46ee0ceb1b0aa494423c98955
Prashant Tripathi <Prashant.Tripathi@unilever.com>
2024-07-29 16:46:17 +0530

    updatye

--- NOTEBOOKS/t.py
+++ NOTEBOOKS/t.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup
 import csv
 
 URL = "https://www.moneycontrol.com/stocks/marketstats/nse-mostactive-stocks/nifty-50-9/"
-OUTPUT_FILE = './post2.csv'
+OUTPUT_FILE = "./post2.csv"
+
 
 def get_perct():
     try:
@@ -11,10 +12,10 @@ def get_perct():
         response.raise_for_status()  # Check for request errors
         html = response.text
 
-        soup = BeautifulSoup(html, 'html.parser')
+        soup = BeautifulSoup(html, "html.parser")
         stock_percents = []
 
-        for element in soup.select('tr td:nth-child(5)'):
+        for element in soup.select("tr td:nth-child(5)"):
             stock_perct = element.get_text()
             stock_percents.append(stock_perct)
 
@@ -23,10 +24,11 @@ def get_perct():
         print(f"Error occurred while hitting URL: {e}")
         return []
 
+
 if __name__ == "__main__":
     stock_percents = get_perct()
     if stock_percents:
-        with open(OUTPUT_FILE, mode='w', newline='') as file:
+        with open(OUTPUT_FILE, mode="w", newline="") as file:
             writer = csv.writer(file)
             for stock_perct in stock_percents:
                 writer.writerow([stock_perct])
