3fb4754dbc70e682be315f66679b548795ea8cda
Prashant Tripathi <Prashant.Tripathi@unilever.com>
2024-08-03 02:48:29 +0530

    update

--- NOTEBOOKS/portfolio.py
+++ NOTEBOOKS/portfolio.py
@@ -5,18 +5,6 @@ import pandas as pd
 from pydantic import BaseModel, field_validator
 from common_utilities import Portfolio, logger, global_path
 
-df_trade_history = pd.read_csv(global_path.tradehistory_silver_file_path)
-
-
-df_trade_history["datetime"] = pd.to_datetime(df_trade_history["datetime"])
-
-
-df_trade_history = df_trade_history.sort_values(by="datetime")
-
-logger.info(
-    f"Read SILVER Layer trade history data from: {global_path.tradehistory_silver_file_path}"
-)
-
 
 class TradeRecord(BaseModel):
     datetime: datetime
@@ -44,27 +32,26 @@ class TradeRecord(BaseModel):
             raise ValueError(f"Invalid expiry date format: {e}")
 
 
-class OpenPosition(BaseModel):
-    open_datetime: datetime
-    side: str
-    quantity: Union[float, int]
-    open_price: Union[float, int]
-    open_amount: Union[float, int]
-
-
-class ClosedPosition(BaseModel):
-    open_datetime: datetime
-    close_datetime: datetime
+class TradePosition(BaseModel):
+    # INFO
+    stock_name: str
     exchange: Optional[str]
     segment: Optional[str]
-    stock_name: str
-    side: str
+    # STATUS
     quantity: Union[float, int]
+    # OPEN INFO
+    open_datetime: datetime
+    open_side: str
     open_price: Union[float, int]
     open_amount: Union[float, int]
-    close_price: Union[float, int]
-    close_amount: Union[float, int]
-    pnl_amount: Union[float, int]
+    # CLOSE INFO
+    close_datetime: Optional[datetime] = None
+    close_side: Optional[str] = None
+    close_price: Optional[Union[float, int]] = None
+    close_amount: Optional[Union[float, int]] = None
+    # PNL INFO
+    pnl_amount: Optional[Union[float, int]] = None
+    pnl_percentage: Optional[Union[float, int]] = None
 
 
 class Stock(BaseModel):
@@ -72,80 +59,90 @@ class Stock(BaseModel):
     exchange: Optional[str] = None
     segment: Optional[str] = None
     expiry_date: Optional[datetime] = None
-
-    holding_quantity: float = 0.0
-    avg_price: float = 0.0
-    holding_amount: float = 0.0
-    OpenPosition: List[Dict[str, OpenPosition]] = []
+    open_positions: List[Dict[str, TradePosition]] = []
+    closed_positions: List[Dict[str, TradePosition]] = []
 
     def trade(self, trade_record: TradeRecord):
         logger.info(trade_record)
+        trade_qt = trade_record.quantity
+        for open_position in self.open_positions:
+            # Matching symbols
+            if (
+                open_position.quantity > 0
+                and open_position.open_side != trade_record.side
+            ):
+                # CLOSING ORDER CALC
+                min_qt = min(trade_record.quantity, open_position.quantity)
+                pnl_amount = (
+                    trade_record.price - open_position.open_price
+                ) * min_qt
+                pnl_percentage = (
+                    pnl_amount / (open_position.open_price * min_qt)
+                ) * 100
 
-        trade_record.quantity = (
-            trade_record.quantity
-            if trade_record.side == "BUY"
-            else (-1) * trade_record.quantity
-        )
+                # STATUS UPDATE
+                open_position.quantity -= min_qt
+                open_position.open_amount = (
+                    open_position.quantity * open_position.open_price
+                )
 
-        if (self.holding_quantity * trade_record.quantity) >= 0:
-
-            pnl_amount = 0
-            pnl_percentage = 0
-
-            self.avg_price = (
-                (self.avg_price * self.holding_quantity)
-                + (trade_record.price * trade_record.quantity)
-            ) / (self.holding_quantity + trade_record.quantity)
-        else:
-
-            pnl_amount = (
-                (trade_record.price - self.avg_price)
-                * min(abs(trade_record.quantity), abs(self.holding_quantity))
-                * (abs(self.holding_quantity) / self.holding_quantity)
-            )
-            pnl_percentage = (
-                pnl_amount
-                / (
-                    self.avg_price
-                    * min(
-                        abs(trade_record.quantity), abs(self.holding_quantity)
+                # Adding result to closed_trade list
+                self.closed_positions.append(
+                    TradePosition(
+                        # INFO
+                        stock_name=trade_record.stock_name,
+                        exchange=trade_record.exchange,
+                        segment=trade_record.segment,
+                        # STATUS
+                        quantity=min_qt,
+                        # OPEN INFO
+                        open_datetime=open_position.open_datetime,
+                        open_side=open_position.open_side,
+                        open_price=open_position.open_price,
+                        open_amount=(open_position.open_price * min_qt),
+                        # CLOSE INFO
+                        close_datetime=trade_record.datetime,
+                        close_side=trade_record.side,
+                        close_price=trade_record.price,
+                        close_amount=(trade_record.price * min_qt),
+                        # PNL INFO
+                        pnl_amount=pnl_amount,
+                        pnl_percentage=pnl_percentage,
                     )
                 )
-            ) * 100
+                # UPDATE LEFT OVER TRADE_QT
+                trade_qt -= min_qt
 
-            if abs(trade_record.quantity) > abs(self.holding_quantity):
-                self.avg_price = trade_record.price
-
-        self.holding_quantity += trade_record.quantity
-        self.holding_amount = self.holding_quantity * self.avg_price
-
-        trade_record.quantity = abs(trade_record.quantity)
-
-        trade_data = trade_record.model_dump()
-        trade_data.update(self.model_dump(exclude=self.trade_history))
-        trade_data.update(
-            {
-                "pnl_amount": pnl_amount,
-                "pnl_percentage": pnl_percentage,
-            }
-        )
-
-        self.trade_history.append(trade_data)
+        if trade_qt > 0:
+            self.open_positions.append(
+                TradePosition(
+                    # INFO
+                    stock_name=trade_record.stock_name,
+                    exchange=trade_record.exchange,
+                    segment=trade_record.segment,
+                    # STATUS
+                    quantity=trade_qt,
+                    # OPEN INFO
+                    open_datetime=trade_record.datetime,
+                    open_side=trade_record.side,
+                    open_price=trade_record.price,
+                    open_amount=trade_qt * trade_record.price,
+                )
+            )
 
     def check_expired(self):
-
         if self.expiry_date and datetime.now() > self.expiry_date:
             self.trade(
                 TradeRecord(
+                    stock_name=self.stock_name,
+                    exchange=self.exchange,
+                    segment=self.segment,
+                    expiry_date=self.expiry_date.date(),
                     datetime=self.expiry_date,
-                    side="SELL" if self.holding_quantity > 0 else "BUY",
+                    side="EXPIRED",
                     quantity=abs(self.holding_quantity),
                     price=0,
                     amount=0,
-                    exchange=self.exchange,
-                    segment=self.segment,
-                    stock_name=self.stock_name,
-                    expiry_date=self.expiry_date.date(),
                 )
             )
 
@@ -178,16 +175,19 @@ class Portfolio:
     def get_trade_history(self):
         trade_history = []
         for stock in self.stocks.values():
-            trade_history += stock.trade_history
+            for each in stock.closed_positions:
+                trade_history.append(each.model_dump())
         return trade_history
 
 
 portfolio = Portfolio()
 
+df_trade_history = pd.read_csv(global_path.tradehistory_silver_file_path)
+
 for record in (
     df_trade_history[
         df_trade_history["stock_name"].isin(
-            ["NIFTY-PE-24650-18JUL2024", "TATAPOWER"]
+            ["TATAPOWER"]  # "NIFTY-PE-24650-18JUL2024",
         )
     ]
     .astype(str)
@@ -195,12 +195,8 @@ for record in (
 ):
     portfolio.trade(TradeRecord(**record))
 
-portfolio.check_expired_stocks()
+# portfolio.check_expired_stocks()
 
-
-df_trade_history = pd.DataFrame(data=portfolio.get_trade_history())
-
-df_trade_history
-
-
-portfolio.get_trade_history()
+df_pnl = pd.DataFrame(data=portfolio.get_trade_history())
+print(df_pnl)
+df_pnl.to_csv("out.csv", index=False)
