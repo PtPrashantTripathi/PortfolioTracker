27b71a27c6e480872bc3816fca51e6b07f23d621
Pt. Prashant tripathi <26687933+PtPrashantTripathi@users.noreply.github.com>
2024-08-09 13:23:39 +0530

    Add files via upload

---
+++ FRONTEND/k.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# URL of the CSV file
+csv_url = 'https://raw.githubusercontent.com/ptprashanttripathi/PortfolioTracker/main/DATA/GOLD/ProfitLoss/ProfitLoss_data.csv'
+
+# Read the CSV file into a DataFrame
+df = pd.read_csv(csv_url)
+
+# Calculate the sum of the 'pnl_amount' column
+total_sum = df['pnl_amount'].sum()
+
+print(f"Total pnl_amount: {total_sum:.2f}")
