e149f57bf2a433503c36cb85285481094ba137b5
Prashant Tripathi <ptprashanttripathi@outlook.com>
2024-08-22 20:46:15 +0530

    prpogress

--- FRONTEND/script.js
+++ FRONTEND/script.js
@@ -72,7 +72,6 @@ async function loadHoldingsTrandsChart(data) {
     // responsive: true,
     chart: {
       type: "area",
-
       height: "100%", // allows flexibility for responsive designs
       width: "100%",
 
@@ -125,7 +124,7 @@ async function loadHoldingsTrandsChart(data) {
       },
     },
     legend: {
-      show: true, // Hide legend
+      show: false, // Hide legend
     },
     grid: {
       show: false, // Hides both x and y axis grid lines
@@ -175,13 +174,6 @@ function parseNum(value) {
     ? parseInt(value)
     : parseFloat(parseFloat(value).toFixed(2));
 }
-// Helper function to create a cell with text content and optional classes
-function createCell(text, classes = []) {
-  const cell = document.createElement("td");
-  cell.innerHTML = text;
-  classes.forEach((cls) => cell.classList.add(cls));
-  return cell;
-}
 
 async function loadProfitLossDataTable(data) {
   const tableBody = document.getElementById("ProfitLossTable");
@@ -190,7 +182,7 @@ async function loadProfitLossDataTable(data) {
     const row = document.createElement("tr");
     const pnl_flag = parseFloat(record.pnl) < 0;
     // Create and append cells
-    row.appendChild(createCell(`${record.symbol} (${record.segment})`));
+    row.appendChild(createCell(`${record.scrip_name} (${record.segment})`));
     row.appendChild(createCell(parseNum(record.quantity)));
     row.appendChild(createCell(priceConvert(record.avg_price)));
     row.appendChild(createCell(priceConvert(record.sell_price)));
@@ -210,77 +202,125 @@ async function loadProfitLossDataTable(data) {
 }
 
 async function loadHoldingsDataTable(data) {
-  const tableBody = document.getElementById("CurrentHoldingsTable");
-  tableBody.innerHTML = ""; // Clear existing table rows
+  const table = document.getElementById("CurrentHoldingsTable"); // <table class="table m-0" id="CurrentHoldingsTable"></table>
+  table.innerHTML = ""; // Clear existing table rows
+
+  // Create and append the table header
+  table.innerHTML = `
+    <thead>
+      <tr>
+        <th>Stock Name</th>
+        <th>Qty.</th>
+        <th>Avg Price</th>
+        <th>Invested</th>
+        <th>CMP Price</th>
+        <th>Current</th>
+        <th>Unrealized PNL</th>
+        <th>Days</th>
+      </tr>
+    </thead>
+  `;
+
+  const tableBody = document.createElement("tbody");
 
   data.forEach((record) => {
     const row = document.createElement("tr");
     const pnl =
       parseFloat(record.close_amount) - parseFloat(record.holding_amount);
-    const pnl_flag = pnl < 0;
+    const pnlClass = pnl < 0 ? "text-danger" : "text-success";
+    const pnlPercentage = `${priceConvert(pnl)} (${
+      pnl >= 0 ? "+" : ""
+    }${parseNum((pnl * 100) / record.holding_amount)}%)`;
 
-    // Create and append cells
-    row.appendChild(createCell(`${record.symbol} (${record.segment})`));
-    row.appendChild(createCell(parseNum(record.holding_quantity)));
-    row.appendChild(createCell(priceConvert(record.avg_price)));
-    row.appendChild(createCell(priceConvert(record.holding_amount)));
+    const cells = [
+      createCell(`${record.scrip_name} (${record.segment})`),
+      createCell(parseNum(record.holding_quantity)),
+      createCell(priceConvert(record.avg_price)),
+      createCell(priceConvert(record.holding_amount)),
+      createCell(priceConvert(record.close_price), [pnlClass]),
+      createCell(priceConvert(record.close_amount), [pnlClass]),
+      createCell(pnlPercentage, [pnlClass]),
+      createCell("TBD"),
+    ];
 
-    row.appendChild(
-      createCell(
-        priceConvert(record.close_price),
-        pnl_flag ? ["text-danger"] : ["text-success"]
-      )
-    );
+    // Append all cells to the row
+    cells.forEach((cell) => row.appendChild(cell));
 
-    row.appendChild(
-      createCell(
-        priceConvert(record.close_amount),
-        pnl_flag ? ["text-danger"] : ["text-success"]
-      )
-    );
-
-    row.appendChild(
-      createCell(
-        `${priceConvert(pnl)} (${pnl_flag ? "+" : ""}${parseNum(
-          (pnl * 100) / record.holding_amount
-        )}%)`,
-        pnl_flag ? ["text-danger"] : ["text-success"]
-      )
-    );
-    row.appendChild(createCell("TBD"));
     // Append the row to the table body
     tableBody.appendChild(row);
   });
+
+  // Append the table body to the table
+  table.appendChild(tableBody);
+}
+
+// Helper function to create a cell with text content and optional classes
+function createCell(text, classes = []) {
+  const cell = document.createElement("td");
+  cell.innerHTML = text;
+  classes.forEach((cls) => cell.classList.add(cls));
+  return cell;
 }
 
 function updateFinancialSummary(data) {
   const { close, holding } = data;
   const pnl = close - holding;
+  const pnlClass = pnl > 0 ? "bg-success" : "bg-danger";
+  const pnlIcon = pnl > 0 ? "▲" : "▼";
+  const pnlPercentage = parseNum((pnl * 100) / holding);
+
+  const summaryItems = [
+    {
+      value: priceConvert(close),
+      label: "Total Assets Worth",
+      colorClass: "bg-info",
+      iconClass: "fas fa-coins",
+      link: "#",
+    },
+    {
+      value: priceConvert(holding),
+      label: "Total Investment",
+      colorClass: "bg-warning",
+      iconClass: "fas fa-cart-shopping",
+      link: "#",
+    },
+    {
+      value: priceConvert(pnl),
+      label: "Total P&L",
+      colorClass: pnlClass,
+      iconClass: "fas fa-chart-pie",
+      link: "#",
+    },
+    {
+      value: `${pnlIcon}${pnlPercentage}%`,
+      label: "Overall Return",
+      colorClass: pnlClass,
+      iconClass: "fas fa-chart-line",
+      link: "#",
+    },
+  ];
 
   const elem = document.getElementById("FinancialSummary");
-  elem.innerHTML = `
-        <!-- All Your Assets Worth -->
-        <div class="col-sm-12 col-md-4 mb-3 mb-md-0">
-            <div class="h4 ${
-              pnl > 0 ? "text-success" : "text-danger"
-            }">${priceConvert(close, true)}</div>
-            <small class="text-secondary">All your assets worth</small>
-        </div>
-        <!-- Invested Amount -->
-        <div class="col-sm-12 col-md-4 mb-3 mb-md-0">
-            <div class="h4 text-primary">${priceConvert(holding, true)}</div>
-            <small class="text-secondary">Invested amount</small>
-        </div>
-        <!-- Overall Returns -->
-        <div class="col-sm-12 col-md-4">
-            <div class="h4 ${
-              pnl > 0 ? "text-success" : "text-danger"
-            }">${priceConvert(pnl, true)} (${pnl > 0 ? "+" : ""}${parseNum(
-    (pnl * 100) / holding
-  )}%)</div>
-            <small class="text-secondary">Overall Returns</small>
-        </div>
-    `;
+  elem.innerHTML = summaryItems
+    .map(
+      (item) => `
+<div class="col-lg-3 col-6">
+  <div class="small-box ${item.colorClass}">
+    <div class="inner">
+      <h4>${item.value}</h4>
+      <h6>${item.label}</h6>
+    </div>
+    <div class="icon">
+      <i class="${item.iconClass}"></i>
+    </div>
+    <a href="${item.link}" class="small-box-footer">
+      More info <i class="fas fa-arrow-circle-right"></i>
+    </a>
+  </div>
+</div>
+`
+    )
+    .join("");
 }
 
 function updateProfitLossDataSummary(data) {
@@ -317,11 +357,6 @@ function find_base_path() {
     return "https://raw.githubusercontent.com/ptprashanttripathi/PortfolioTracker/main/";
   } else {
     return "/";
-    // document.body.innerHTML = `
-    //     <div style="text-align: center; padding: 50px;">
-    //         <h1>Error: Unsupported Domain</h1>
-    //         <p>The application is not supported on this domain.</p>
-    //     </div>`;
   }
 }
 
@@ -340,6 +375,7 @@ async function main() {
   const holdingsDataFilePath = `${base_path}DATA/GOLD/Holdings/Holdings_data.csv`;
   const holdingsData = await getData(holdingsDataFilePath);
   const filterdData = findMaxDateRecords(holdingsData);
+  console.log(filterdData);
   loadHoldingsDataTable(filterdData);
 
   // Fetch the data using getData
