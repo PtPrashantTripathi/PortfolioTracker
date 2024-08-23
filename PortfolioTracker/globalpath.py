ee4b8f6bd7e88db42d9a66832b1080bbba58236e
Prashant Tripathi <Prashant.Tripathi@unilever.com>
2024-08-23 13:53:45 +0530

    * * * * 6

--- PortfolioTracker/globalpath.py
+++ PortfolioTracker/globalpath.py
@@ -1,20 +1,23 @@
 import os
 from pathlib import Path
+
 from dotenv import load_dotenv
 
 load_dotenv()
+
 
 class GlobalPath:
     """
     A Global Paths Class for managing global paths for various data layers and files.
     """
+
     def __init__(self):
         """
         Initializes a new GlobalPath instance and sets up directory paths.
         Uses PROJECT_DIR environment variable if defined, else defaults to a local path.
         """
         # Use PROJECT_DIR if available, otherwise use the default local path
-        
+
         self.base_path = Path(
             os.getenv(
                 "PROJECT_DIR",  # Get the GitHub workspace path from the environment variable
@@ -33,7 +36,7 @@ class GlobalPath:
         """
         # Construct the full path by joining the base path with the source path
         full_path = self.base_path.joinpath(source_path)
-        
+
         # Check if the path is for a file (contains a dot in the name)
         if "." in full_path.name:
             # Ensure the directory for the file exists
@@ -41,7 +44,7 @@ class GlobalPath:
         else:
             # Ensure the directory itself exists
             full_path.mkdir(parents=True, exist_ok=True)
-        
+
         return full_path
 
 
@@ -50,7 +53,9 @@ if __name__ == "__main__":
     global_path = GlobalPath()
 
     # Create a path for the TradeHistory source layer
-    tradehistory_source_layer_path = global_path.joinpath("DATA/SOURCE/TradeHistory")
+    tradehistory_source_layer_path = global_path.joinpath(
+        "DATA/SOURCE/TradeHistory"
+    )
 
     # Print the generated path
     print(f"TradeHistory Source Layer Path: {tradehistory_source_layer_path}")
@@ -60,4 +65,3 @@ if __name__ == "__main__":
         print(f"Path exists: {tradehistory_source_layer_path}")
     else:
         print(f"Path does not exist: {tradehistory_source_layer_path}")
-
