53bff1f887acc60d61a38abc3c65150e57e0f036
ptprashanttripathi <ptprashanttripathi@outlook.com>
2024-08-28 18:16:59 +0530

    one odne

--- FRONTEND/script.js
+++ FRONTEND/script.js
@@ -1,7 +1,8 @@
 // Function to parse CSV data
 function parseCSV(csv) {
     const lines = csv.trim().split("\n");
-    const headers = lines[0].split(",").map((header) => header.trim()); // Trim all headers
+    const headers = lines[0].split(",").map((header) => header.trim());
+    // Trim all headers
     const result = [];
 
     for (let i = 1; i < lines.length; i++) {
@@ -31,7 +32,8 @@ async function getData(filePath) {
         // Handle CSV file
         if (filePath.endsWith(".csv")) {
             const csvData = await response.text();
-            const parsedData = parseCSV(csvData); // Parse the CSV data
+            const parsedData = parseCSV(csvData);
+            // Parse the CSV data
             return parsedData;
 
             // Handle JSON file
@@ -72,7 +74,8 @@ async function loadHoldingsTrandsChart(data) {
         // responsive: true,
         chart: {
             type: "area",
-            height: "100%", // allows flexibility for responsive designs
+            height: "100%",
+            // allows flexibility for responsive designs
             width: "100%",
 
             toolbar: {
@@ -124,10 +127,12 @@ async function loadHoldingsTrandsChart(data) {
             },
         },
         legend: {
-            show: false, // Hide legend
+            show: false,
+            // Hide legend
         },
         grid: {
-            show: false, // Hides both x and y axis grid lines
+            show: false,
+            // Hides both x and y axis grid lines
         },
     };
 
@@ -245,7 +250,8 @@ async function loadProfitLossDataTable(data) {
 }
 
 async function loadCurrentHoldingsDataTable(data) {
-    const table = document.getElementById("CurrentHoldingsTable"); // <table class="table m-0" id="CurrentHoldingsTable"></table>
+    const table = document.getElementById("CurrentHoldingsTable");
+    // <table class="table m-0" id="CurrentHoldingsTable"></table>
 
     // Create and append the table header
     table.innerHTML = `
@@ -309,8 +315,7 @@ function createCell(text, classes = []) {
     return cell;
 }
 
-function updateFinancialSummary(data) {
-    const { current_value, invested_value, overall_pnl } = data;
+function updateFinancialSummary(current_value, invested_value, overall_pnl) {
     const pnlClass = overall_pnl > 0 ? "bg-success" : "bg-danger";
     const pnlIcon = overall_pnl > 0 ? "▲" : "▼";
 
@@ -337,7 +342,7 @@ function updateFinancialSummary(data) {
             link: "#",
         },
         {
-            value: `${pnlIcon}${parseNum(
+            value: `${pnlIcon} ${parseNum(
                 (overall_pnl * 100) / invested_value
             )}%`,
             label: "Overall Return",
@@ -370,35 +375,37 @@ function updateFinancialSummary(data) {
         .join("");
 }
 
-function updateProfitLossDataSummary(data) {
-    const { sold, invested, pnl } = data;
-
-    const pnlPercentage = (pnl * 100) / invested;
-
+function updateProfitLossDataSummary(sold, invested, pnl) {
+    const pnlClass = pnl > 0 ? "bg-success" : "bg-danger";
+    const pnlIcon = pnl > 0 ? "▲" : "▼";
     const summaryItems = [
         {
             value: priceFormat(sold),
-            label: "TOTAL ASSETS TURNOVER",
-            iconColorClass: "bg-info",
-            numberClass: pnl > 0 ? "text-success" : "text-danger",
+            label: "Total Turnover",
+            colorClass: "bg-info",
+
             iconClass: "fas fa-solid fa-coins",
             link: "#",
         },
         {
             value: priceFormat(invested),
-            label: "TOTAL INVESTED",
-            iconColorClass: "bg-warning",
-            numberClass: "text-info",
+            label: "Total Invested",
+            colorClass: "bg-warning",
+
             iconClass: "fas fa-piggy-bank",
             link: "#",
         },
         {
-            value: `${priceFormat(pnl)} (${pnl > 0 ? "+" : ""}${parseNum(
-                pnlPercentage
-            )}%)`,
-            label: "OVERALL REALIZED PNL",
-            iconColorClass: pnl > 0 ? "bg-success" : "bg-danger",
-            numberClass: pnl > 0 ? "text-success" : "text-danger",
+            value: priceFormat(pnl),
+            label: "Total P&L",
+            colorClass: pnlClass,
+            iconClass: "fas fa-chart-pie",
+            link: "#",
+        },
+        {
+            value: `${pnlIcon} ${parseNum((pnl * 100) / invested)}%`,
+            label: "Overall Return",
+            colorClass: pnlClass,
             iconClass: "fas fa-percent",
             link: "#",
         },
@@ -408,13 +415,18 @@ function updateProfitLossDataSummary(data) {
     elem.innerHTML = summaryItems
         .map(
             (item) => `
-            <div class="col-lg-4 col-12 p-0">
-            <div class="info-box m-0">
-            <span class="info-box-icon ${item.iconColorClass} elevation-1"><i class="${item.iconClass}"></i></span>
-            <div class="info-box-content">
-                <span class="info-box-text">${item.label}</span>
-                <span class="info-box-number ${item.numberClass}">${item.value}</span>
-            </div>
+            <div class="col-lg-3 col-6">
+            <div class="small-box ${item.colorClass}">
+                <div class="inner">
+                <h4>${item.value}</h4>
+                <h6>${item.label}</h6>
+                </div>
+                <div class="icon">
+                <i class="${item.iconClass}"></i>
+                </div>
+                <a href="${item.link}" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+                </a>
             </div>
             </div>
             `
@@ -434,21 +446,26 @@ async function main() {
     const base_path = find_base_path();
 
     // Fetch the data using getData
-    const holdingsTrandsFilePath = `${base_path}/DATA/API/HoldingsTrands_data.json`;
-    const holdingsTrands_data = await getData(holdingsTrandsFilePath);
-    loadHoldingsTrandsChart(holdingsTrands_data);
+    const holdingsDataFilePath = `${base_path}/DATA/API/Holdings_data.json`;
+    const holdings_data = await getData(holdingsDataFilePath);
 
-    // Fetch the data using getData
-    const currentHoldingsDataFilePath = `${base_path}/DATA/API/CurrentHoldings_data.json`;
-    const currentHoldingsData = await getData(currentHoldingsDataFilePath);
-    loadCurrentHoldingsDataTable(currentHoldingsData.data);
-    updateFinancialSummary(currentHoldingsData);
+    updateFinancialSummary(
+        holdings_data.current_value,
+        holdings_data.invested_value,
+        holdings_data.overall_pnl
+    );
+    loadCurrentHoldingsDataTable(holdings_data.current_holding);
+    loadHoldingsTrandsChart(holdings_data.holdings_trands);
 
     // Fetch the data using getData
     const profitLossDataFilePath = `${base_path}/DATA/API/ProfitLoss_data.json`;
     const profitLossData = await getData(profitLossDataFilePath);
     loadProfitLossDataTable(profitLossData.data);
-    updateProfitLossDataSummary(profitLossData);
+    updateProfitLossDataSummary(
+        profitLossData.sold,
+        profitLossData.invested,
+        profitLossData.pnl
+    );
 }
 
 main();
