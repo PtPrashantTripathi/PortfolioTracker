1299dfedb26b857e380fc4702df4ab123b0bf564
ptprashanttripathi <ptprashanttripathi@outlook.com>
2024-09-06 11:31:57 +0530

    gp

--- ETLTOOLS/globalpath/__init__.py
+++ ETLTOOLS/globalpath/__init__.py
@@ -1,6 +1,6 @@
 import os
 from typing import List
-from pathlib import Path, PosixPath, WindowsPath
+from pathlib import Path
 from datetime import datetime
 
 from dotenv import load_dotenv
@@ -11,7 +11,7 @@ load_dotenv()
 __all__ = ["GlobalPath"]
 
 
-class GlobalPath(Path):
+class GlobalPath():
     """
     A custom path class that automatically prefixes paths with the PROJECT_DIR environment variable.
     This class extends the built-in pathlib.Path and adds custom methods for path manipulation.
@@ -25,26 +25,19 @@ class GlobalPath(Path):
 
     Methods
     -------
-    createpath(*other_paths):
-        Extends the current path with additional segments.
-    relative_path():
-        Returns the path relative to the PROJECT_DIR for better readability.
     root_path():
         Returns the PROJECT_DIR path as a resolved Path object.
     ensure_exists(full_path):
         Ensures that the directory for the given path exists, creating it if necessary.
     """
 
-    # Ensure compatibility with both Windows and POSIX systems
-    _flavour = WindowsPath._flavour if os.name == "nt" else PosixPath._flavour
-
-    def __new__(cls, *source_path):
+    def __init__(self, source_path):
         """
         Creates a new GlobalPath object.
 
         Parameters
         ----------
-        *source_path : str
+        source_path : str
             The path to be joined with the PROJECT_DIR.
 
         Returns
@@ -53,55 +46,10 @@ class GlobalPath(Path):
             An instance of the GlobalPath class with the resolved path.
         """
         # Create the full path by joining the PROJECT_DIR with the source path
-        full_path = cls.root_path().joinpath(*source_path)
+        self.path = self.root_path().joinpath(source_path).resolve()
 
         # Ensure the directory for the path exists
-        cls.ensure_exists(full_path)
-
-        # Return an instance of the correct Path subclass
-        return super().__new__(cls, full_path)
-
-    def createpath(self, *other_paths):
-        """
-        Extends the current path with additional segments.
-
-        Parameters
-        ----------
-        *other_paths : str
-            Additional paths to join with the current path.
-
-        Returns
-        -------
-        GlobalPath
-            A new GlobalPath object with the additional paths joined.
-        """
-        # Construct the full path by joining the base path with the additional segments
-        full_path = self.joinpath(*other_paths)
-
-        # Ensure the directory for the path exists
-        self.ensure_exists(full_path)
-
-        return GlobalPath(full_path)
-
-    def relative_path(self):
-        """
-        Returns the path relative to the PROJECT_DIR for better readability.
-
-        Returns
-        -------
-        str
-            The path relative to the PROJECT_DIR environment variable in Unix format.
-        """
-        root_path = self.root_path()
-        # If PROJECT_DIR is not set or the current path is not under it, return the full path
-        if not root_path or not str(self).startswith(str(root_path)):
-            return self.as_posix()
-
-        # Calculate the relative path and convert it to Unix format
-        relative_path = str(self)[
-            len(str(root_path)) + 1 :
-        ]  # +1 to skip the separator
-        return Path(relative_path).as_posix()
+        self.ensure_exists(self.path)
 
     @staticmethod
     def root_path():
@@ -153,14 +101,14 @@ class GlobalPath(Path):
         file_paths = []
 
         # Iterate over all files in the directory and subdirectories
-        for path in self.rglob(file_pattern):
+        for path in self.path.rglob(file_pattern):
             if path.is_file():
                 file_modified_time = datetime.fromtimestamp(
                     os.path.getmtime(path)
                 )
                 # Check if file was modified after the given timestamp
                 if file_modified_time > timestamp:
-                    file_paths.append(self.joinpath(path))
+                    file_paths.append(path)
 
         # Log the number of detected files
         num_files = len(file_paths)
@@ -178,18 +126,16 @@ if __name__ == "__main__":
     tradehistory_source_layer_path = GlobalPath("DATA/BRONZE/TradeHistory")
 
     # Print the generated path
-    print(
-        f"TradeHistory Source Layer Path: {tradehistory_source_layer_path.relative_path()}"
-    )
+    print(f"Root Path: {tradehistory_source_layer_path.root_path()}")
+    print(f"Source Path: {tradehistory_source_layer_path}")
 
     # Check if the path exists and print its existence status
-    if tradehistory_source_layer_path.exists():
-        print(
-            f"Path exists: {tradehistory_source_layer_path.relative_path()} in root {tradehistory_source_layer_path.root_path().as_posix()}"
-        )
+    if tradehistory_source_layer_path.path.exists():
+        print("Path exists")
     else:
-        print(
-            f"Path does not exist: {tradehistory_source_layer_path.relative_path()}"
-        )
+        print("Path does not exist")
     for each in tradehistory_source_layer_path.check_files_availability():
-        print(each.relative_path())
+        print(each)
+
+    new_path = tradehistory_source_layer_path.path.joinpath("new")
+    print(type(new_path), new_path)
