d5f7d73e2182f48157b80a4a1c2e0db32aaea301
ptprashanttripathi <ptprashanttripathi@outlook.com>
2024-09-27 10:54:00 +0530

    deepshikha

--- PIPELINE/all_runner.sh
+++ PIPELINE/all_runner.sh
@@ -13,32 +13,31 @@ export PYDEVD_DISABLE_FILE_VALIDATION=1
 # Function to execute notebook and log output
 execute_notebook() {
     # Execute the notebook
-    layer=$1    # arguments : Layer names
-    notebook=$2 # arguments : notebook names
-    echo "Executing ${layer}/${notebook}.ipynb ..."
-    # python -m
-    jupyter nbconvert --execute --to notebook --inplace "NOTEBOOKS/${layer}/${notebook}.ipynb"
+    notebook=$1 # arguments : notebook path
+    jupyter nbconvert --execute --to notebook --inplace "${notebook}"
     echo "---------------------------------------------"
 }
 
+
 # 01_BRONZE_LAYER_ETL
-execute_notebook "01_BRONZE_LAYER_ETL" "01_TradeHistory"
-execute_notebook "01_BRONZE_LAYER_ETL" "02_StockData"
-execute_notebook "01_BRONZE_LAYER_ETL" "03_Symbol"
+execute_notebook ./NOTEBOOKS/01_BRONZE_LAYER_ETL/01_TradeHistory.ipynb
+# execute_notebook ./NOTEBOOKS/01_BRONZE_LAYER_ETL/02_StockData.ipynb
+execute_notebook ./NOTEBOOKS/01_BRONZE_LAYER_ETL/03_Symbol.ipynb
 
 # 02_SILVER_LAYER_ETL
-execute_notebook "02_SILVER_LAYER_ETL" "01_Symbol"
-execute_notebook "02_SILVER_LAYER_ETL" "02_TradeHistory"
-execute_notebook "02_SILVER_LAYER_ETL" "03_StockPrice"
-execute_notebook "02_SILVER_LAYER_ETL" "04_StockEvents"
+execute_notebook ./NOTEBOOKS/02_SILVER_LAYER_ETL/01_Symbol.ipynb
+execute_notebook ./NOTEBOOKS/02_SILVER_LAYER_ETL/02_TradeHistory.ipynb
+execute_notebook ./NOTEBOOKS/02_SILVER_LAYER_ETL/03_StockPrice.ipynb
+execute_notebook ./NOTEBOOKS/02_SILVER_LAYER_ETL/04_StockEvents.ipynb
 
 # 03_GOLD_LAYER_ETL
-execute_notebook "03_GOLD_LAYER_ETL" "01_Holding"
-execute_notebook "03_GOLD_LAYER_ETL" "02_ProfitLoss"
-execute_notebook "03_GOLD_LAYER_ETL" "03_Dividend"
+execute_notebook ./NOTEBOOKS/03_GOLD_LAYER_ETL/01_Holding.ipynb
+execute_notebook ./NOTEBOOKS/03_GOLD_LAYER_ETL/02_ProfitLoss.ipynb
+execute_notebook ./NOTEBOOKS/03_GOLD_LAYER_ETL/03_Dividend.ipynb
 
 # 04_API_LAYER_ETL
-execute_notebook "04_API_LAYER_ETL" "01_API"
+execute_notebook ./NOTEBOOKS/04_API_LAYER_ETL/01_API.ipynb
 
 # 05_PRESENTATION_LAYER (commented out, uncomment if needed)
-# execute_notebook "05_PRESENTATION_LAYER" "01_Trands"
+# execute_notebook ./NOTEBOOKS/05_PRESENTATION_LAYER/01_Trands.ipynb
+
