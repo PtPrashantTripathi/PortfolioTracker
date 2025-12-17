d98bde22df45e6f106f4c299ba8db8f4e328d142
ptprashanttripathi <ptprashanttripathi@outlook.com>
2025-12-17 21:17:50 +0530

    TTOLS

--- tools/all_runner.sh
+++ tools/all_runner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Shell script for Linux/macOS
-# Location : ./SCRIPTS/all_runner.sh
+# Location : ./tools/all_runner.sh
 
 # DATA CLEAN UP
 # rm -rf DATA/BRONZE
@@ -13,17 +13,18 @@ export PYDEVD_DISABLE_FILE_VALIDATION=1
 
 # Function to execute notebook and log output
 execute_notebook() {
+    # arguments : notebook path
+    notebook=$1
     # Execute the notebook
-    notebook=$1 # arguments : notebook path
     jupyter nbconvert --execute --to notebook --inplace "${notebook}"
     echo "---------------------------------------------"
 }
 
 
 # 01_BRONZE_LAYER_ETL
-execute_notebook ./NOTEBOOKS/01_BRONZE_LAYER_ETL/01_TradeHistory.ipynb
-execute_notebook ./NOTEBOOKS/01_BRONZE_LAYER_ETL/02_StockData.ipynb
-execute_notebook ./NOTEBOOKS/01_BRONZE_LAYER_ETL/03_Symbol.ipynb
+execute_notebook ./NOTEBOOKS/01_BRONZE_LAYER_ETL/01_Symbol.ipynb
+execute_notebook ./NOTEBOOKS/01_BRONZE_LAYER_ETL/02_TradeHistory.ipynb
+execute_notebook ./NOTEBOOKS/01_BRONZE_LAYER_ETL/03_StockData.ipynb
 
 # 02_SILVER_LAYER_ETL
 execute_notebook ./NOTEBOOKS/02_SILVER_LAYER_ETL/01_Symbol.ipynb
@@ -32,9 +33,8 @@ execute_notebook ./NOTEBOOKS/02_SILVER_LAYER_ETL/03_StockPrice.ipynb
 execute_notebook ./NOTEBOOKS/02_SILVER_LAYER_ETL/04_StockEvents.ipynb
 
 # 03_GOLD_LAYER_ETL
-execute_notebook ./NOTEBOOKS/03_GOLD_LAYER_ETL/01_Holding.ipynb
-execute_notebook ./NOTEBOOKS/03_GOLD_LAYER_ETL/02_ProfitLoss.ipynb
-execute_notebook ./NOTEBOOKS/03_GOLD_LAYER_ETL/03_Dividend.ipynb
+execute_notebook ./NOTEBOOKS/03_GOLD_LAYER_ETL/01_Portfolio.ipynb
+execute_notebook ./NOTEBOOKS/03_GOLD_LAYER_ETL/02_Dividend.ipynb
 
 # 04_API_LAYER_ETL
 execute_notebook ./NOTEBOOKS/04_API_LAYER_ETL/01_API.ipynb
